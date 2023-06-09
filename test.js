5440777a2189abb9bb358fc03d48896a7f0c1912
Shelly Sachdev <77107785+Shelly1986@users.noreply.github.com>
2023-06-09 10:55:39 +0200

    Add files via upload

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ import { Typer } from "./util.js"
 
 window.addEventListener("DOMContentLoaded", async event => {
   const element = document.getElementById('testdiv')
-  await Typer.display(element, 'Sarah continues her quest to unravel the secrets of the Galactic Keystone. As she explores further, she finds a hidden chamber with an ancient inscription. The inscription reads: "I am taken from a mine, and shut up in a wooden case, from which I am never released, and yet I am used by almost every person. What am I?"');
+  await Typer.display(element, 'Sarah continues her quest to unravel the secrets of the Galactic Keystone. As she explores further, she finds a hidden chamber with an ancient inscription. The inscription reads:');
   
   for (let node of document.querySelectorAll(".hidden")) {
     node.classList.remove("hidden")
